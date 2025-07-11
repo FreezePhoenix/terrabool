45793d870d43aae3eb6cc65d0391a4d2814c421f
FreezePhoenix <24441367+FreezePhoenix@users.noreply.github.com>
2025-07-11 13:42:46 -0400

    Create Queue.js
    Part of a rework for https://github.com/Filipssh/terrabool/pull/3

---
+++ js/model/Queue.js
@@ -0,0 +1,31 @@
+/**
+ * Author: FreezePhoenix
+ * Description: A quick-n-dirty queue. The queue is implemented as a singly linked list that keeps track of the head and tail of the list.
+ * The property "next" is set on data that is passed into the queue, so users may want to pre-set this field to null on any objects they pass in.
+ * Setting the field to null helps performance slightly, but is not truly necessary.
+ * The queue's goal is to minimize memory usage while maintaining efficiency.
+ */
+class Queue {
+    head = null;
+    tail = null;
+    constructor() {}
+    enqueue(data) {
+        if(this.head == null) {
+            this.head = this.tail = data;
+        } else {
+            this.tail.next = data;
+            this.tail = data;
+        }
+    }
+    dequeue() {
+      if(this.head == null) {
+          return null;
+      }
+      let temp = this.head;
+      this.head = temp.next;
+      return temp;
+    }
+    empty() {
+      return this.head == null;
+    }
+}
